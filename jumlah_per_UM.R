ce09b6b4fb298e1454e24e7200c3705566a601ae
robbybinsar <robbybinsar@gmail.com>
2020-07-14 11:09:39 +0700

    Perdana

---
+++ jumlah_per_UM.R
@@ -0,0 +1,13 @@
+output <- data.frame(matrix(ncol=7,nrow=7))
+colnames(output) <- c("Amfibi","Aves", "Insekta", "Mamalia","Myriapoda","Pisces","Reptil")
+rownames(output) <- c("ANJA","ANJAS","SMM","KAL","PPM","PMP","ANJAP")
+input <- function(x) {
+  ## x adalah nama data framenya, data frame harus di WD
+  dat <- read.csv(x)
+  for(i i
+    
+  }
+}
+
+length(which(x[,index kolom]=="Taxon"))
+##di atas ini untuk cari berapa
