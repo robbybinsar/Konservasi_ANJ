55b78e6b2100e86750906acbbe696b8781429759
robbybinsar <robbybinsar@gmail.com>
2021-08-03 19:23:06 +0700

    cs

--- temp_script_analysis/matching_cs_between_years.R
+++ temp_script_analysis/matching_cs_between_years.R
@@ -5,28 +5,35 @@ library(openxlsx)
 UM <- c("SMM", "ANJA", "ANJAS", "ANJAP", "PMP", "PPM", "KAL")
 
 for (i in UM) {
-  myarray <- read.xlsx("C:/Users/robby/OneDrive - PT. Austindo Nusantara Jaya Tbk/SUMMARY PENDAKI/2020/CITIZEN SCIENCE 2020.xlsx",
+  myarray <- read.xlsx("C:/Users/robby/OneDrive - PT. Austindo Nusantara Jaya Tbk/SUMMARY PENDAKI/2021/CITIZEN SCIENCE 2021.xlsx",
             sheet = i, startRow = 3, cols = 2)
   pertama <- nrow(myarray)
-  myarray2020 <- myarray$Nama
-  lkupvalue <- read.xlsx("C:/Users/robby/OneDrive - PT. Austindo Nusantara Jaya Tbk/SUMMARY PENDAKI/2019/CITIZEN SCIENCE 2019.xlsx",
+  lkupvalue <- read.xlsx("C:/Users/robby/OneDrive - PT. Austindo Nusantara Jaya Tbk/SUMMARY PENDAKI/2020/CITIZEN SCIENCE 2020.xlsx",
                          sheet = i, startRow = 3, cols = 2)
   lkupvalue <- lkupvalue$Nama
-  for(y in lkupvalue) {
-    hitung <- stringsim(y, myarray2020)
-    if (max(hitung) < 0.75) {
-      myarray <- myarray %>% add_row(Nama = y)
-    } 
+  lkupvalue2 <- read.xlsx("C:/Users/robby/OneDrive - PT. Austindo Nusantara Jaya Tbk/SUMMARY PENDAKI/2019/CITIZEN SCIENCE 2019.xlsx",
+                          sheet = i, startRow = 3, cols = 2)
+  lkupvalue2 <- lkupvalue2$Nama
+  gabungan <- list(x = lkupvalue, y = lkupvalue2)
+  var <- c("x", "y")
+  for(z in var) {
+    for(y in gabungan[[z]]) {
+      hitung <- stringsim(y, myarray$Nama)
+      if (max(hitung) < 0.75) {
+        myarray <- myarray %>% add_row(Nama = y)
+      } 
+    }
   }
+  
   kedua <- nrow(myarray)
-  write.csv(myarray, paste0("./cs_2019-2020Jul/", i,".csv"))
+  write.csv(myarray, paste0("./cs_2019-2021/2019 - 2021/", i,".csv"))
   print(paste(i, "bertambah", kedua-pertama))
 }
 
 # Total akumulasi
 output <- data.frame(matrix(nrow = 0, ncol = 0))
 for (i in UM) {
-  df <- read.csv(paste0("./cs_2019-2020Jul/",i,".csv"))
+  df <- read.csv(paste0("./cs_2019-2021/2019 - 2021/",i,".csv"))
   output <- bind_rows(output, df)
 }
 
